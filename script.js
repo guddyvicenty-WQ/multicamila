4251b99fab5f2e9bf786ea309f0d92be51ef5ecc
St1v0n 12 <guddyesteven123@gmail.com>
2026-07-30 21:25:23 -0400

    modificacion de mi css y js

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 const body = document.querySelector('body');
-const botonModo = document.querySelector('#botonModo');
+const botonModo = document.querySelector('#btn-tema');
 
-let esDeDia = false;
+let esOscuro = false;
+
+function alternarModo() {
+    body.classList.toggle('oscuro');
+    esOscuro = !esOscuro;
+    
+    if (esOscuro) {
+        botonModo.textContent = '🌙 Cambiar a modo claro';
+    } else {
+        botonModo.textContent = '☀️ Cambiar a modo oscuro';
+    }
+}
+
+botonModo.addEventListener('click', alternarModo);
