d3f7a320623961a8763803e1dcb2702356bdcb05
St1v0n 12 <guddyesteven123@gmail.com>
2026-08-04 20:31:07 -0400

    agragado de validaciones

--- script.js
+++ script.js
@@ -27,3 +27,30 @@ function cambiarTexto() {
 
 const botonTexto = document.querySelector('#btn-tema2');
 botonTexto.addEventListener('click', cambiarTexto);
+
+const botonform = document.querySelector('#form-contacto');
+const aviso = document.querySelector('#mensaje-error');
+
+function revisarFormulario(event) {
+    event.preventDefault();
+
+    const nombre = document.querySelector('#nombre').value;
+    const mensaje = document.querySelector('#mensaje').value;
+    const correo = document.querySelector('#email').value;
+
+    if (nombre === '' || mensaje === '') {
+        aviso.textContent = 'Por favor completa todos los campos';
+        aviso.classList.add('error');
+        aviso.classList.remove('exito');
+    } else if (correo.includes('@')=== false) {
+        aviso.textContent = 'Por favor ingresa un correo válido';
+        aviso.classList.add('error');
+        aviso.classList.remove('exito');
+    } else {
+        aviso.textContent = 'Formulario enviado con exito';
+        aviso.classList.add('exito');
+        aviso.classList.remove('error');
+    }
+}
+
+botonform.addEventListener('submit', revisarFormulario);
