fdc1d2e23d1f1f0a7f43b9b8a23a88715ad91a94
St1v0n 12 <guddyesteven123@gmail.com>
2026-08-11 20:03:37 -0400

    inicio del js

---
+++ parcial2/script.js
@@ -0,0 +1,7 @@
+const form = document.querySelector("#form-cita");
+const aviso = document.querySelector("#aviso-cita");
+
+function validarFormulario(event) {
+    const nombre = document.querySelector("#nombre").value.trim();
+    const correo = document.querySelector("#correo").value.trim();
+}
