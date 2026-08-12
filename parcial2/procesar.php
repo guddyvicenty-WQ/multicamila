6df6d44d42370599703f9b28775b6455d807a636
St1v0n 12 <guddyesteven123@gmail.com>
2026-08-11 20:20:06 -0400

    prueba php

--- parcial2/procesar.php
+++ parcial2/procesar.php
@@ -12,5 +12,21 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     echo "<h1>cita reservada en optica mirasol</h1>";
     echo "<hr>";
 
+    echo "<h2>Datos de la cita</h2>";
+    echo "<p><strong>Nombre:</strong> $nombre</p>";
+    echo "<p><strong>Correo:</strong> $correo</p>";
+    echo "<p><strong>Consulta:</strong> $consulta</p>";
     
+    echo "<hr>";
+
+    echo "<h2>Nuestros servicios</h2>";
+    echo "<ul>";
+    foreach ($servicios as $servicio) {
+        echo "<li>$servicio</li>";
+    }
+    echo "</ul>";
+    echo "<hr>";
+
+    echo "<p><strong>Te atiende steven</strong></p>";
+    echo "<p><small>Parcial 2 - steven - 11 de agosto de 2026</small></p>";
 }
