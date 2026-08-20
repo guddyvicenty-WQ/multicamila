eeecef23829fc759ca2b13fcb950dbe8c64945fb
St1v0n 12 <guddyesteven123@gmail.com>
2026-08-20 19:34:36 -0400

    colocalo en funcion down

--- definitivo/database/migrations/2026_08_20_232903_add_stock_to_solicituds_table.php
+++ definitivo/database/migrations/2026_08_20_232903_add_stock_to_solicituds_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('solicituds', function (Blueprint $table) {
-            //
+            $table->dropColumn('stock');
         });
     }
 };
